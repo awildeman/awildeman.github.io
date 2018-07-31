ec4b49647c231107fd249572408ec3b0fa78b114
Leah Belyea <leahbelyea@gmail.com>
2018-07-31 20:31:33 -0300

    package name, form success and responsiveness, style tweaks, google analytics

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // Mobile menu
   $('.open-menu').click(function() {
     $('nav').addClass('open');
   });
@@ -7,11 +9,20 @@ $(document).ready(function() {
     $('nav').removeClass('open');
   });
 
+  // Close menu on esc
   $(document).keydown(function(event) {
     if (event.keyCode === 27) {
       $('nav').removeClass('open');
     }
   });
 
+  // Form success message
+  var formQueryString = window.location.href.match(/form=submitted/);
+  var formSubmitted = formQueryString !== null && formQueryString.length > 0;
+  if (formSubmitted) {
+    $('.form-success').show().delay(5000).fadeOut(400);
+  }
+
+  // Form validation
   $('form').validate();
 });
